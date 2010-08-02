74bfabc30b57a0304887a257be882e8ae962c7bd
Markus Hjort <markus.hjort@reaktor.fi>
2010-08-02 17:25:34 +0300

    Added dependency to Concordion

--- project/build/ConcotraProject.scala
+++ project/build/ConcotraProject.scala
@@ -2,14 +2,13 @@ import sbt._
 class ConcotraProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
   // scalatra
-  val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at
-"https://oss.sonatype.org/content/repositories/snapshots"
-  val sonatypeNexusReleases = "Sonatype Nexus Releases" at
-"https://oss.sonatype.org/content/repositories/releases"
+  val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  val sonatypeNexusReleases = "Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases"
   val scalatra = "org.scalatra" %% "scalatra" % "2.0.0-SNAPSHOT"
  
   // jetty
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test"
-  val servletApi = "org.mortbay.jetty" % "servlet-api" %
-"2.5-20081211" % "provided"
+  val servletApi = "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "provided"
+
+  val concordion = "org.concordion" % "concordion" % "1.3.1" % "test"
 }
