17eda38cd6143547d085261eaf2b4013aee82faa
tkiuru <tiina.kiuru@reaktor.fi>
2010-08-05 16:53:05 +0300

    Test that almost tests something

--- src/test/scala/concotra/ExampleTest.scala
+++ src/test/scala/concotra/ExampleTest.scala
@@ -4,5 +4,9 @@ import org.concordion.integration.junit4.ConcordionRunner
 import org.junit.runner.RunWith
 
 @RunWith(classOf[ConcordionRunner])
-class ExampleTest {}
+class ExampleTest {
+	def getGreeting() : String = {
+		"Hello Markustiina!"
+	}
+}
 
