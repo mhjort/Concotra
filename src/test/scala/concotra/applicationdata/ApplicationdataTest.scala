eb7faa1d40d97c0b5edd828abdb86f95a1c76b65
Markus Hjort <markus.hjort@reaktor.fi>
2010-09-06 16:25:07 +0300

    Parse JSon to case class

--- src/test/scala/concotra/applicationdata/ApplicationdataTest.scala
+++ src/test/scala/concotra/applicationdata/ApplicationdataTest.scala
@@ -4,15 +4,19 @@ import concotra._
 import dispatch._
 import Http._
 import dispatch.json.JsHttp._
+import net.liftweb.json.JsonParser._
 
+  case class Person(age: String)
 class ApplicationdataTest extends ConcordionSuite { 
+  implicit val formats = net.liftweb.json.DefaultFormats
   def doTheTrick() = {
     val http = new Http
     
     val req = new Request("http://localhost:8080")
     val rform = req << Map("age" -> "31")
-    val age = http(rform / "api" ># { 'age ! str })
-     Console.println(age)
+    val response = http(rform / "api" >~ { _.mkString })
+    val person = parse(response).extract[Person]
+    Console.println(person)
   }
 }
 
