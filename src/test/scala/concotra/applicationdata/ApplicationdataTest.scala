40555ab9202daebb4e4067a980b63346fc7978fb
Markus Hjort <markus.hjort@reaktor.fi>
2010-09-07 10:17:25 +0300

    Refactoring

--- src/test/scala/concotra/applicationdata/ApplicationdataTest.scala
+++ src/test/scala/concotra/applicationdata/ApplicationdataTest.scala
@@ -3,20 +3,24 @@ package concotra.applicationdata
 import concotra._
 import dispatch._
 import Http._
-import dispatch.json.JsHttp._
 import net.liftweb.json.JsonParser._
 
-  case class Person(age: String)
+case class Person(age: String)
+
 class ApplicationdataTest extends ConcordionSuite { 
   implicit val formats = net.liftweb.json.DefaultFormats
+
   def doTheTrick() = {
-    val http = new Http
-    
-    val req = new Request("http://localhost:8080")
-    val rform = req << Map("age" -> "31")
-    val response = http(rform / "api" >~ { _.mkString })
+    val response = post(Map("age" -> "31"))
     val person = parse(response).extract[Person]
     Console.println(person)
+  }
+
+  def post(params : Map[String, String]) = {
+    val http = new Http
+    val req = new Request("http://localhost:8080")
+    val rform = req << params
+    http(rform / "api" >~ { _.mkString })
   }
 }
 
