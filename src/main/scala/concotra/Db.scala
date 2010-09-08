64a900c546fd519b02fafe4a3de6a5a71644b66d
Markus Hjort <markus.hjort@reaktor.fi>
2010-09-08 10:54:13 +0300

    Added cleanup to db

--- src/main/scala/concotra/Db.scala
+++ src/main/scala/concotra/Db.scala
@@ -1,21 +1,34 @@
 package concotra
 
 import scala.io.Source
+import java.io._
 
 class Db {
   private val dbDir = "db"
 
-  def nextId = new java.io.File(dbDir).listFiles.size + 1
+  private def nextId = new File(dbDir).listFiles.size + 1
 
+  private def initialize { 
+    val db = new File(dbDir)
+    if (!db.exists) db.mkdir
+  }
+ 
   def readEntry(id : String) = {
+    initialize
     Source.fromFile(dbDir + "/" + id).mkString
   }
 
-  def writeEntry(contents : String) = {
+  def newEntry(contents : String) = {
+    initialize
     val id = nextId
-    val writer = new java.io.FileWriter(dbDir + "/" + nextId)
+    val writer = new FileWriter(dbDir + "/" + id)
     writer.write(contents)
     writer.flush
     id
+  }
+
+  def cleanUp() {
+    initialize
+    new File("db").listFiles.foreach(f => f.delete)
   }
 }
