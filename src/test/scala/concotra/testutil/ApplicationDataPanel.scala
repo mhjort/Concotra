076b89b061544eef1a81726da4c3f69cece4a6ee
Markus Hjort <markus.hjort@reaktor.fi>
2010-09-13 14:50:48 +0300

    Implemented Ajax wait

--- src/test/scala/concotra/testutil/ApplicationDataPanel.scala
+++ src/test/scala/concotra/testutil/ApplicationDataPanel.scala
@@ -1,16 +1,20 @@
 package concotra.testutil
 
-import org.openqa.selenium.By
+import org.openqa.selenium.{By,WebDriver}
+import org.openqa.selenium.support.ui.{WebDriverWait, ExpectedCondition}
+
+class ElementExistsCondition(id : String) extends ExpectedCondition[Boolean] {
+  override def apply(driver : WebDriver) = !Driver.get().findElements(By.id(id)).isEmpty
+}
 
 object ApplicationDataPanel {
-
-  def setValidSalary() {setSalary("9500")}
+  def setValidSalary() { setSalary("9500") }
 
   def setSalary(salary : String) {findElement("salary").sendKeys(salary)}
 
   def acceptApplication() {
     findElement("submit").click
-    Thread.sleep(100)
+    new WebDriverWait(Driver.get(), 5).until(new ElementExistsCondition("status"))
   }
 
   def getApplicationId = findElement("savedApplicationId").getText
