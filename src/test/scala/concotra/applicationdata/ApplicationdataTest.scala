1530d7b5a3b97fbdedd460bf968e6e40335d7f36
Markus Hjort <markus.hjort@reaktor.fi>
2010-09-14 11:06:26 +0300

    Cleaned up ApplicationDataTest

--- src/test/scala/concotra/applicationdata/ApplicationdataTest.scala
+++ src/test/scala/concotra/applicationdata/ApplicationdataTest.scala
@@ -1,25 +1,15 @@
 package concotra.applicationdata
 
 import concotra._
-import dispatch._
-import Http._
-import net.liftweb.json.JsonParser._
-
-case class ApplicationData(status: String)
+import concotra.testutil._
 
 class ApplicationdataTest extends ConcordionSuite { 
-  implicit val formats = net.liftweb.json.DefaultFormats
-
-  def createApplication(salary : String, firstEmploymentStartDate:String) = {
-    val response = post(Map("salary" -> salary, "firstEmploymentStartDate" -> firstEmploymentStartDate))
-    parse(response).extract[ApplicationData].status
-  }
-  
-  def post(params : Map[String, String]) = {
-    val http = new Http
-    val req = new Request("http://localhost:8080")
-    val rform = req << params
-    http(rform / "api" >~ { _.mkString })
+  def createApplication(salary: String, 
+                        firstEmploymentStartDate: String, 
+                        applicationReceivedDate: String, 
+                        unpromptedPayer: String, 
+                        declarationMethod: String) = {
+    new Application(salary, firstEmploymentStartDate, applicationReceivedDate, unpromptedPayer, declarationMethod).accept.status
   }
 }
 
---
+++ src/test/scala/concotra/testutil/Application.scala
@@ -0,0 +1,30 @@
+package concotra.testutil
+
+import dispatch._
+import Http._
+import net.liftweb.json.JsonParser._
+
+case class ApplicationData(status: String)
+
+case class Application(salary: String,
+                       firstEmploymentStartDate: String, 
+                       applicationReceivedDate: String, 
+                       unpromptedPayer: String, 
+                       declarationMethod: String) {
+  def accept = {
+    implicit val formats = net.liftweb.json.DefaultFormats
+    val response = post(Map("salary" -> salary, 
+			    "firstEmploymentStartDate" -> firstEmploymentStartDate,
+                            "applicationReceivedDate" -> applicationReceivedDate,
+                            "unpromptedPayer" -> unpromptedPayer,
+                            "declarationMethod" -> declarationMethod))
+    parse(response).extract[ApplicationData]
+  }
+  
+  def post(params : Map[String, String]) = {
+    val http = new Http
+    val req = new Request("http://localhost:8080")
+    val rform = req << params
+    http(rform / "api" >~ { _.mkString })
+  }
+}
