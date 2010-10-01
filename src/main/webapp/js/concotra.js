5f42b631763bb659b2434a8c8445adfebf8640b1
tkiuru <tiina.kiuru@reaktor.fi>
2010-10-01 12:23:47 +0300

    Removed maksusuunnitelma and made feedback prettier

--- src/main/webapp/js/concotra.js
+++ src/main/webapp/js/concotra.js
@@ -3,7 +3,7 @@ $(function() {
     $.post("http://localhost:8080/api", { "salary": $("#salary").val(), "firstEmploymentStartDate": $("#firstEmploymentStartDate").val() },
       function(application) {
         if (application.status == '1') {
-          $("#status").html('Tallennettu <span id="savedApplicationId">' + application.id + '</span>');
+          $("#status").html('<span class="success">Tallennettu <span id="savedApplicationId">' + application.id + '</span></span>');
 	} else {
 	  $("#status").html('<span class="error">Virheellinen päivämäärä</span>');
 	}
