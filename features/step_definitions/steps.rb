0f4af4ff8cc69deb96f667214db936be486f4fca
tkiuru <tiina.kiuru@reaktor.fi>
2010-11-01 16:04:54 +0200

    Semi working Application data e2e Cucumber spec

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,22 +1,25 @@
 require "selenium-webdriver"
 
 Given /^the application data form is opened$/ do
-  driver = Selenium::WebDriver.for :firefox
-  driver.navigate.to "http://localhost:8080"
-  driver.find_element(:id, 'salary').send_keys("9500")
-  driver.find_element(:id, 'submit').click
+  @driver = Selenium::WebDriver.for :firefox
+  @driver.navigate.to "http://localhost:8080"
+end                                                                                                                                                                                            
+When /^application handler enters following information$/ do |values|
+  row = values.hashes[0]
+  enter_value('salary', row["Salary"])
+  enter_value('firstEmploymentStartDate', row["First employment start date"])
+  find_by_id('submit').click
+  sleep(1)
+end                                                                                                                                                                                            
+Then /^incomplete application is created$/ do
+  puts find_by_id('savedApplicationId').text
+  @driver.quit                                                                                                                                               
+end  
 
-  sleep(2000)
-  puts driver.title
-  driver.quit                                                                                                                                               
-end                                                                                                                                                                                            
-                                                                                                                                                                                               
-When /^application handler enters following information$/ do |table|                                                                                                                           
-  # table is a Cucumber::Ast::Table                                                                                                                                                            
-  puts table["salary"]
-  pending # express the regexp above with the code you wish you had                                                                                                                            
-end                                                                                                                                                                                            
-                                                                                                                                                                                               
-Then /^incomplete application is created$/ do                                                                                                                                                  
-  pending # express the regexp above with the code you wish you had                                                                                                                            
-end      
+def find_by_id(id)
+  @driver.find_element(:id, id) 
+end   
+
+def enter_value(id, value)
+  find_by_id(id).send_keys value
+end 
