77f1a4b8548e305f59b540ca448cf6ce56cc161f
Markus Hjort <markus.hjort@reaktor.fi>
2010-11-01 15:32:42 +0200

    Stuff before my keyboard crashed

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,17 +1,19 @@
 require "selenium-webdriver"
 
 Given /^the application data form is opened$/ do
-
   driver = Selenium::WebDriver.for :firefox
   driver.navigate.to "http://localhost:8080"
+  driver.find_element(:id, 'salary').send_keys("9500")
+  driver.find_element(:id, 'submit').click
 
+  sleep(2000)
   puts driver.title
-
   driver.quit                                                                                                                                               
 end                                                                                                                                                                                            
                                                                                                                                                                                                
 When /^application handler enters following information$/ do |table|                                                                                                                           
   # table is a Cucumber::Ast::Table                                                                                                                                                            
+  puts table["salary"]
   pending # express the regexp above with the code you wish you had                                                                                                                            
 end                                                                                                                                                                                            
                                                                                                                                                                                                
