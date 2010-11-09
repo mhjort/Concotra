15d58969926c71512be8fceda820b9d39d1330f2
Markus Hjort <markus.hjort@reaktor.fi>
2010-11-09 14:44:28 +0200

    Refined test case descriptions and implemented first API test

--- features/ApplicationData.feature
+++ features/ApplicationData.feature
@@ -7,12 +7,25 @@ Given the application data form is opened
 When the insurance clerk enters following information
 	|Salary|First employment start date|Application arrival date|Declaration method|
 	|9500  |1.1.2010		   |1.5.2010		    |MONTHLY	       |  
+And submits the form
 Then incomplete application is created
 
-Scenario: Salary - API
+Scenario Outline: Earliest possible first employment start date is 1.1.1961
+Given the insurance clerk has given valid data
+When she gives <First employment start date> as first employment start date
+And accepts the data
+Then the result is <Result>
 
-Given application handler has given otherwise valid data
-When she gives <Salary>	
+Examples:
+	|First employment start date|Result|
+	|15.9.2009|OK|
+	|1.1.1961|OK|
+	|31.12.1960|ERROR|
+
+Scenario: Salary has to be positive number
+Given the insurance clerk has given valid data
+When she gives <Salary>
+And accepts the data	
 Then the result is <Result>
 	|Salary		|Result	|
 	|9500 		|OK	|
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,19 +7,29 @@ Given /^the application data form is opened$/ do
   @driver = Selenium::WebDriver.for :firefox
   @driver.navigate.to "http://localhost:8080"
 end                                                                                                                                                                                            
+
 When /^the insurance clerk enters following information$/ do |values|
   row = values.hashes[0]
   enter_value('salary', row["Salary"])
   enter_value('firstEmploymentStartDate', row["First employment start date"])
+end 
+
+When /^submits the form$/ do
   find_by_id('submit').click
   sleep(1)
-end  
+end       
 
-Given /^the insurance clerk has given otherwise valid data$/ do                                                                                                                                
-  response = RestClient.post 'http://localhost:8080/api', :salary => '9600', :firstEmploymentStartDate => '1.2.2010'                                                                          
-  puts response.to_str
+When /^accepts the data$/ do
+  @response = RestClient.post 'http://localhost:8080/api', @valid_data
+end       
+
+Given /^the insurance clerk has given valid data$/ do
+  @valid_data = { :salary => '9600', :firstEmploymentStartDate => '1.2.2010' }
 end            
 
+When /^she gives (.*) as first employment start date$/ do |first_employment_start_date|
+  @valid_data[:firstEmploymentStartDate] =  first_employment_start_date
+end      
                                                                                                                                                                                           
 Then /^incomplete application is created$/ do
   id = find_by_id('savedApplicationId').text
@@ -27,6 +37,12 @@ Then /^incomplete application is created$/ do
   saved_status = read_db_entry(id)["status"]
   saved_status.should == "1"
 end  
+
+Then /^the result is (.*)$/ do |result|
+  status = JSON.parse(@response)["status"]
+  if result == "OK" then status.should == "1" end
+  if result == "ERROR" then status.should == "0" end
+end
 
 def read_db_entry(id) 
   entry = IO.read("db/" + id)
