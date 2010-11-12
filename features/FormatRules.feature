0bfe62e0dfacbe2539d92afda746fe4708ea2a17
tkiuru <tiina.kiuru@reaktor.fi>
2010-11-12 18:03:57 +0200

    Test and implementation for money format rules

--- features/FormatRules.feature
+++ features/FormatRules.feature
@@ -14,3 +14,16 @@ Examples:
 	|text|FORMAT_ERROR|
 	|15/09/2009|FORMAT_ERROR|
 
+
+Scenario Outline: Money format is NN,NN or NN.NN
+Given the insurance clerk has given valid data
+When she gives <Money> as salary
+And accepts the data
+Then the result is <Result>
+
+Examples:
+	|Money|Result|
+	|12345,12|OK|
+	|12345.12|OK|
+	|text|FORMAT_ERROR|
+	|12345|OK|
--- src/main/scala/concotra/ConcotraServlet.scala
+++ src/main/scala/concotra/ConcotraServlet.scala
@@ -4,16 +4,18 @@ import org.scalatra._
 import net.liftweb.json.JsonDSL._
 import net.liftweb.json._
 import org.joda.time.format.DateTimeFormat
+import java.text.{DecimalFormat,ParseException}
 
 class ConcotraServlet extends ScalatraServlet with UrlSupport {
   before {
     contentType = "application/json"
   }
  
-  post("/api") {
+  post("/api") { 
     try {
       val firstEmploymentStartDate = toDate(params("firstEmploymentStartDate"))
-      if (firstEmploymentStartDate.isBefore(toDate("1.1.1961")) || new java.math.BigDecimal(params("salary")).doubleValue() < 0)
+      val salary = toMoney(params("salary"))
+      if (firstEmploymentStartDate.isBefore(toDate("1.1.1961")) || salary < 0) 
         compact(JsonAST.render(("status" -> "BUSINESS_RULE_VIOLATED")))
       else {
         val id = Db.newEntry(compact(JsonAST.render(
@@ -25,13 +27,29 @@ class ConcotraServlet extends ScalatraServlet with UrlSupport {
         compactResponse(id, "OK")
       }
     } catch {
-      case _: IllegalArgumentException => compact(JsonAST.render(("status" -> "FORMAT_ERROR")))
+      case _: FormatException => compact(JsonAST.render(("status" -> "FORMAT_ERROR")))
     }
    }
 
   def compactResponse(id: Int, status: String) = compact(JsonAST.render(("status" -> status) ~ ("id" -> id)))
  
-  def toDate(date : String) = DateTimeFormat.forPattern("dd.MM.yyyy").parseDateTime(date)
+  def toDate(date : String) = {
+    try {    
+      DateTimeFormat.forPattern("dd.MM.yyyy").parseDateTime(date)
+    } catch {
+        case _: IllegalArgumentException => throw new FormatException
+    }
+  }
+  
+  def toMoney(money : String) = {
+    try {    
+      new DecimalFormat("0,00").parse(money).doubleValue()
+    } catch {
+        case _: ParseException => throw new FormatException
+    }
+  }
 
   protected def contextPath = request.getContextPath
 }
+
+class FormatException extends RuntimeException {}
