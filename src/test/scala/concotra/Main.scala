6ec25cf78f8cdd1cb5b8c64538b1a1988198c0f9
Markus Hjort <markus.hjort@reaktor.fi>
2011-04-14 16:31:26 +0300

    Main method takes port as command line parameter

--- src/test/scala/concotra/Main.scala
+++ src/test/scala/concotra/Main.scala
@@ -3,14 +3,16 @@ package concotra
 import org.mortbay.jetty.Server
 import org.mortbay.jetty.webapp.WebAppContext
 
-object Main extends Application {
-  val server = new Server(8080)
-  val context = new WebAppContext()
-  context.setDescriptor("webapp/WEB-INF/web.xml")
-  context.setResourceBase("webapp")
-  context.setContextPath("/")
-  context.setParentLoaderPriority(true)
-  server.setHandler(context)
-  server.start()
-  server.join()
+object Main  {
+  def main(args:Array[String]) = {
+    val server = new Server(Integer.parseInt(args(0)))
+    val context = new WebAppContext()
+    context.setDescriptor("webapp/WEB-INF/web.xml")
+    context.setResourceBase("webapp")
+    context.setContextPath("/")
+    context.setParentLoaderPriority(true)
+    server.setHandler(context)
+    server.start()
+    server.join()
+  }
 }
